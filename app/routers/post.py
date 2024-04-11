151d084d92b41e1d877f4d0537cf716d33e63942
vipin tiwari <vipin.tiwari@devslane.com>
2024-04-11 18:31:30 +0530

    post-crud operation

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter;
+from fastapi import APIRouter, Depends;
+from sqlalchemy.orm import Session;
+
 # schemas
-from ..schemas.post import PostResponse;
+from ..schemas.post import PostResponse, PostCreate;
+# db
+from ..get_db import get_db;
+# services
+from ..services import post as postService
 
 
 router= APIRouter(
@@ -8,19 +14,22 @@ router= APIRouter(
 )
 
 
-@router.get('/posts', response_model= list[PostResponse])
-def get_posts():
-    return []
 
-@router.get('/posts/{post_id}', response_model= PostResponse)
-def get_post(post_id: int):
-    return {}
+@router.get('/posts', response_model= list[PostResponse])
+def get_posts(db:Session= Depends(get_db) ):
+    return postService.get_posts(db)
+
+
+@router.get('/posts/{id}', response_model= PostResponse)
+def get_post(id: int, db:Session= Depends(get_db) ):
+    return postService.get_post(db, id)
 
 
 @router.post('/posts', response_model= PostResponse)
-def create_post():
-    return {}
+def create_post(post: PostCreate, db: Session= Depends(get_db) ):
+    return postService.create_post(db, post)
 
-@router.delete('/posts/{post_id}', response_model= str)
-def delete_post(post_id: int):
-    return 'success'
+
+@router.delete('/posts/{id}', response_model= str)
+def delete_post(post_id: int, db: Session= Depends(get_db) ):
+    return postService.delete_post(db)
---
+++ app/services/post.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import Session;
+# models
+from ..models import Post;
+# schemas
+from ..schemas.post import PostCreate;
+
+def get_posts(db: Session):
+    return db.query(Post).all()
+
+
+def get_post(db: Session, id: int):
+    return db.query(Post).filter(Post.id== id).first()
+
+
+def create_post(db: Session, post: PostCreate):
+    new_post= Post(title= post.title, content= post.content, is_published= post.is_published, owner_id= post.owner_id)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+
+def delete_post(db: Session, id: int):
+    post= db.get(Post, id)
+    db.delete(post)
+    db.commit()
+    return 'success'
